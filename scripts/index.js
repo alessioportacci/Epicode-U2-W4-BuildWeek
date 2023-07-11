5ec1709ec9b0ecf7df5d2d11e409dca3595e6b9d
Yanina <yaninaaguero@hotmail.it>
2023-07-11 15:14:08 +0200

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -18,46 +18,47 @@ const getRemoteData = function () {
     })
 
     .then((elements) => {
-      console.log(elements);
+      console.log("elements", elements);
       let spinnerContainer = document.getElementById("spinner-container");
       spinnerContainer.classList.add("d-none");
       let row = document.getElementById("row-to-append-children");
-      elements.forEach((element) => {
-        let newCol = document.createElement("div");
-        newCol.classList.add("col", "col-4");
-        newCol.innerHTML = `
-                         <div class="card" style="width: 18rem;">
-                   <img src="${element.img}" class="card-img-top" alt="copertina">
-                   <div class="card-body">
-                     <h5 class="card-title">${element.title}</h5>
-                     <p class="card-text">${element.price}</p>
-                     <a href="#" class="btn btn-primary" id='delete-button'>Scarta</a>
-                     <a href="#" class="btn btn-primary" id='buy-now'>Compra ora</a>
-                   </div>
-                 </div>
+      const myElements= function() {
+        for(let i=0; i>elements.length; i++)
+          let newCol = document.createElement("div");
+          newCol.classList.add("col", "col-4");
+          newCol.innerHTML = `
+          <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
                          `;
-        row.appendChild(newCol);
-        const deleteButton = function () {
-          const dButton = document.getElementById("delete-button");
-          newCol.classList.add("d-none");
-        };
-        const buyMovies = function () {
-          const buyButton = document.getElementById("buy-now");
-          const getUl = document.querySelector("ul");
-          const newLi = document.createElement("li");
-          newLi.innerHTML = `<h4>${element.title}</h4>
-                    <p>${element.price}</p>`;
-          getUl.appendChild(newLi);
-          localStorage.setItem("Elementi nel carrello", element.title);
-        };
+          row.appendChild(newCol);
+        })
+        // const deleteButton = function () {
+        //   const dButton = document.getElementById("delete-button");
+        //   newCol.classList.add("d-none");
+        // }
+        // const buyMovies = function () {
+        //   const buyButton = document.getElementById("buy-now");
+        //   const getUl = document.querySelector("ul");
+        //   const newLi = document.createElement("li");
+        //   newLi.innerHTML = `<h4>${element.title}</h4>
+        //             <p>${element.price}</p>`;
+        //   getUl.appendChild(newLi);
+        //   localStorage.setItem("Elementi nel carrello", element.title);
+        // };
 
-        newCol.addEventListener("click", deleteButton);
-        newCol.addEventListener("click", buyMovies);
-      });
-    })
+        // newCol.addEventListener("click", deleteButton);
+        // newCol.addEventListener("click", buyMovies);
 
-    .catch((err) => {
-      console.log("Errore!", err);
+        .catch((err) => {
+          console.log("Errore!", err);
+        });
     });
 };
+
 getRemoteData();
