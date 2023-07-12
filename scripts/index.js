9462a1a94e8651548e870d44e7f87632c43c14fc
Yanina <yaninaaguero@hotmail.it>
2023-07-12 12:31:04 +0200

    Percorso per pagina Album

--- scripts/index.js
+++ scripts/index.js
@@ -11,7 +11,6 @@ const getRemoteData = function () {
       "Content-Type": "application/jason",
     },
   })
-
     .then((res) => {
       if (res.ok) return res.json();
       else throw new Error("Errore nella chiamata");
@@ -21,33 +20,38 @@ const getRemoteData = function () {
       console.log(elements);
       spinnerContainer.classList.toggle("d-none");
       const row = document.getElementById("row-to-append-children");
-      elements.data.forEach((element) => 
-      {
+      elements.data.forEach((element) => {
         let newCol = document.createElement("div");
         newCol.classList.add("col", "col-3", "my-5");
         newCol.innerHTML = `<div class="card h-100 bg-dark" style=";">
                               <img src="${element.album.cover}" class="card-img-top p-2" alt="copertina">
                               <div class="card-body">
-                                <h5 class="card-title" id="card-title">${element.title}</h5>
+                                <h5 class="card-title album-redirect" id="card-title" value="${element.album.id}">${element.title}</h5>
                                 <p class="card-text artist-redirect text-hover" id="card-text" value="${element.artist.id}">${element.artist.name}</p>
                               </div>
-                            </div>`
+                            </div>`;
         row.appendChild(newCol);
-      })
+      });
     })
-      
+
     .then(() => {
       //Per ogni elemento classe .artist-redirect mettiamo un redirect a artist.html con l'id contenuto nel suo value
-      document.querySelectorAll(".artist-redirect").forEach(artist =>
-        artist.addEventListener("click", function(){
-          location.href = "artist.html" + "?id=" + artist.getAttribute("value")
-        }
-        ))
-    }) 
+      document.querySelectorAll(".artist-redirect").forEach((artist) =>
+        artist.addEventListener("click", function () {
+          location.href = "artist.html" + "?id=" + artist.getAttribute("value");
+        })
+      );
+      // Percorso per arrivare alla pagina album tramite l'id
+      document.querySelectorAll(".album-redirect").forEach((title) =>
+        title.addEventListener("click", function () {
+          location.href = "album.html" + "?id=" + title.getAttribute("value");
+        })
+      );
+    })
 
     .catch((err) => {
-        console.log("Errore!", err);
-      });
+      console.log("Errore!", err);
+    });
 };
 
 getRemoteData();
