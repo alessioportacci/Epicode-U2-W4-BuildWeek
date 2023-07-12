9b65bd397494f011056145f713d1c405d15774b3
alessioportacci <alessio.portacci.s@gmail.com>
2023-07-12 12:02:38 +0200

    Fix reindirizzamento artisti

--- scripts/index.js
+++ scripts/index.js
@@ -11,6 +11,7 @@ const getRemoteData = function () {
       "Content-Type": "application/jason",
     },
   })
+
     .then((res) => {
       if (res.ok) return res.json();
       else throw new Error("Errore nella chiamata");
@@ -19,30 +20,34 @@ const getRemoteData = function () {
     .then((elements) => {
       console.log(elements);
       spinnerContainer.classList.toggle("d-none");
-      let row = document.getElementById("row-to-append-children");
-      elements.data.forEach((element) => {
+      const row = document.getElementById("row-to-append-children");
+      elements.data.forEach((element) => 
+      {
         let newCol = document.createElement("div");
         newCol.classList.add("col", "col-3", "my-5");
-        newCol.innerHTML = `
-                         <div class="card h-100 bg-dark" style=";">
-                            <img src="${element.album.cover}" class="card-img-top p-2" alt="copertina">
-                            <div class="card-body">
-                              <h5 class="card-title" id="card-title">${element.title}</h5>
-                              <a href="../album.html"><p class="card-text" id="card-text">${element.artist.name}</p></a>
-                            </div>
-                          </div>
-                         `;
+        newCol.innerHTML = `<div class="card h-100 bg-dark" style=";">
+                              <img src="${element.album.cover}" class="card-img-top p-2" alt="copertina">
+                              <div class="card-body">
+                                <h5 class="card-title" id="card-title">${element.title}</h5>
+                                <p class="card-text artist-redirect text-hover" id="card-text" value="${element.artist.id}">${element.artist.name}</p>
+                              </div>
+                            </div>`
         row.appendChild(newCol);
-        const artistsId = elements.data.map((element) => {
-          return element.id;
-        });
-        console.log("artisti id", artistsId);
-      });
+      })
+    })
+      
+    .then(() => {
+      //Per ogni elemento classe .artist-redirect mettiamo un redirect a artist.html con l'id contenuto nel suo value
+      document.querySelectorAll(".artist-redirect").forEach(artist =>
+        artist.addEventListener("click", function(){
+          location.href = "artist.html" + "?id=" + artist.getAttribute("value")
+        }
+        ))
+    }) 
 
-      // .catch((err) => {
-      //   console.log("Errore!", err);
-      // });
-    });
+    .catch((err) => {
+        console.log("Errore!", err);
+      });
 };
 
 getRemoteData();
