c2f309328846075208cbe7eec36e98009256c617
Yanina <yaninaaguero@hotmail.it>
2023-07-13 14:58:13 +0200

    Cards fine

--- scripts/index.js
+++ scripts/index.js
@@ -22,12 +22,19 @@ const getRemoteData = function () {
       const row = document.getElementById("row-to-append-children");
       elements.data.forEach((element) => {
         let newCol = document.createElement("div");
-        newCol.classList.add("col", "col-3", "my-5");
+        newCol.classList.add(
+          "col-6",
+          "col-md-3",
+          "my-3",
+          "p-5",
+          "p-md-1",
+          "h-100"
+        );
         newCol.innerHTML = `<div class="bg-spotify-card spotify-card"> 
         <div class="card h-100 bg-card bg-light-subtle-hover my-cards">
         <div class="p-2 d-flex">
-                              <img src="${element.album.cover_big}" class="card-img-top position-relative spotify-card-image" alt="copertina">
-                              <img src="../assets/imgs/logo.png" class="position-absolute">
+                              <img src="${element.album.cover_big}" class="card-img-top spotify-card-image z-1 position-relative" alt="copertina">
+                              <img src="../assets/imgs/logo.png" class="z-2 position-absolute">
                               </div>
                               <div class="card-body">
                                 <h5 class="card-title album-redirect text-hover text-truncate" id="card-title" value="${element.album.id}">${element.title}</h5>
