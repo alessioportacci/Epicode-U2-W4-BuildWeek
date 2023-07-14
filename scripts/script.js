1c3c5cca9b303de3e66da1e802e1c2975f030359
alessioportacci <alessio.portacci.s@gmail.com>
2023-07-14 15:02:02 +0200

    X cliccabile

--- scripts/script.js
+++ scripts/script.js
@@ -12,3 +12,11 @@ function numberWithCommas(number)
         parts[0]=parts[0].replace(/\B(?=(\d{3})+(?!\d))/g,".");
         return parts.join(",");
     }
+
+const closeFriendPanel = document.querySelector("#activities").lastElementChild.lastElementChild
+closeFriendPanel.setAttribute("style", "cursor:pointer")
+
+closeFriendPanel.addEventListener("click", function(){
+    document.querySelector("#right-column").classList.toggle("d-lg-block")
+    document.querySelector("#central-column").classList.toggle("col-lg-10")
+})
